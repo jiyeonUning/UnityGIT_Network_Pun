43247026a0dc48839331f682f2dd8bf74c5ac168
jiyeonUning <yeon14735@gmail.com>
2024-11-13 16:04:27 +0900

    게임 오브젝트 변수 동기화 예제 코드

--- Assets/_Study/241113/PlayerController.cs
+++ Assets/_Study/241113/PlayerController.cs
@@ -1,30 +1,51 @@
 using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // MonoBehaviourPun : 포톤 뷰에 달려 있어, 네크워크 오브젝트를 이용할 수 있는 기능이 들어 있음.
 // IPunObservable : 포톤 뷰가 자동으로 참조할 수 있게끔 해줄 수 있다.
 public class PlayerController : MonoBehaviourPun, IPunObservable
 {
-    public int value1;
-    public float value2;
-    public float value3;
+    [SerializeField] Color color;
+    [SerializeField] Material mater;
+    private Color OG;
+
+    void Start() { OG = mater.color; }
+    void OnApplicationQuit() { mater.color = OG; }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mater.color = Random.ColorHSV();
+        }
+    }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(value1);
-            stream.SendNext(value2);
-            stream.SendNext(value3);
+            /* stream.SendNext(value1);
+               stream.SendNext(value2);
+               stream.SendNext(value3); */
+
+            stream.SendNext(mater.color.r);
+            stream.SendNext(mater.color.g);
+            stream.SendNext(mater.color.b);
         }
+
         else if (stream.IsReading)
         {
-            value1 = (int)stream.ReceiveNext();
-            value2 = (float)stream.ReceiveNext();
-            value3 = (float)stream.ReceiveNext();
+            // IsReading에선 IsWriting입력값에서 적용된 순서대로 값을 읽어내려 적용하므로,
+            // 원하는 곳에 원하는 값을 입력하려면, IsWriting에서 작성한 순서대로 입력하여야만 한다.
+            /* value1 = (int)stream.ReceiveNext();
+               value2 = (float)stream.ReceiveNext();
+               value3 = (float)stream.ReceiveNext(); */
+
+            color.r = (float)stream.ReceiveNext();
+            color.g = (float)stream.ReceiveNext();
+            color.b = (float)stream.ReceiveNext();
+
+            mater.color = color;
         }
-        
     }
 }
