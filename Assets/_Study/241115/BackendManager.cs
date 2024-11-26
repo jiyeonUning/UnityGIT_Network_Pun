1aed028c1887edd39de9249d20c25bb8f3da4acd
jiyeonUning <yeon14735@gmail.com>
2024-11-27 01:50:37 +0900

    네트워크 데이터 드리븐 : 데이터 베이스 내에 저장된 값 불러오기&읽어들이기 활용 코드 구성

--- Assets/_Study/241115/BackendManager.cs
+++ Assets/_Study/241115/BackendManager.cs
@@ -2,6 +2,7 @@ using Firebase;
 using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Extensions;
+using System;
 using UnityEngine;
 
 public class BackendManager : MonoBehaviour
@@ -20,6 +21,17 @@ public class BackendManager : MonoBehaviour
 
     private void Awake()
     {
+        /* 한 객체에 필요한 데이터를 직접 지정하는 방식의 예시 01
+        JsonTest test = new JsonTest();
+        test.name = "test";
+        test.level = 30;
+        test.rate = 2.3f;
+        
+        test.userData = new UserData();
+        test.userData.phoneNumber = "1234567890";
+        test.userData.address = "서울 강동구 경일 게임 아카데미 숍"; 
+        */
+
         SetSingleton();
     }
 
@@ -63,6 +75,23 @@ public class BackendManager : MonoBehaviour
                 database = FirebaseDatabase.DefaultInstance;
 
                 Debug.Log("Firebase dependencies check success");
+
+
+                // 추가로 데이터 베이스에 저장된 특정 값을 가져오기 위해, 가장 최상위 위치에 존재하는 데이터 값을 가져와 저장한다.
+                // (= https://fir-study-32514-default-rtdb.asia-southeast1.firebasedatabase.app/ <- 해당 경로의 바로 아래 존재하는 값)
+                DatabaseReference root = BackendManager.Database.RootReference;
+
+                // 최상위 데이터 값을 불러오고, 해당 데이터 아래로 저장된 하위 데이터를 타고 내려가, 원하는 값을 이름으로 불러올 수 있다.
+                // ex) 유저 '지연'의 레벨 데이터 정보는 = 최상위 데이터의 자식 UserData의 자식 Jiyeon의 자식 level에 저장되어 있다.
+                DatabaseReference JiyeonLevel = root.Child("UserData").Child("Jiyoen").Child("level");
+
+                // + (데이터 명).OrderByChild = 해당 데이터 값 아래 저장된 하위 데이터들을 모두 가져올 수 있다.
+                //   (데이터 명).Parent = 해당 데이터 값의 상위에 위치한 데이터 값을 가져올 수 있다.
+
+
+                // ~ 데이터 베이스에 저장된 값을 변경하는 방법 ~
+                // 해당 레퍼런스 경로에 저장된 값을 ( )의 값으로 변경해줄 수 있다.
+                JiyeonLevel.SetValueAsync(10);
             }
             // 사용이 가능하지 않다면,
             else
@@ -77,4 +106,24 @@ public class BackendManager : MonoBehaviour
             }
         });
     }
+
+    /* 한 객체에 필요한 데이터를 직접 지정하는 방식의 예시 02
+
+    [Serializable]
+    public class JsonTest
+    {
+        public string mane;
+        public int level;
+        public float rate;
+
+        public UserData userData;
+    }
+
+    [Serializable]
+    public class UserData
+    {
+        public string phoneNumber;
+        public string address;
+    }
+    */
 }
--- Assets/_Study/241118/DatabaseTester.cs
+++ Assets/_Study/241118/DatabaseTester.cs
@@ -11,12 +11,21 @@ public class DatabaseTester : MonoBehaviour
     DatabaseReference userDataRef;
     DatabaseReference levelRef;
 
+
     private void OnEnable()
     {
+        // 현재 인증된 이메일로 로그인 된 유저의 아이디 정보를 저장
         string uid = BackendManager.Auth.CurrentUser.UserId;
+
+        // 위에서 저장한 값을 기반으로, UserData 하위에 위치한 현재 유저 데이터 경로를 저장
         userDataRef = BackendManager.Database.RootReference.Child("UserData").Child(uid);
+
+        // 위에서 저장한 경로 아래에 level 이름을 가진 값을 저장함
         levelRef = userDataRef.Child("level");
 
+
+        // ~ 데이터 읽기 : GetValueAsync ~
+        // 읽어들이는 데이터의 하위에 위치하는 모든 데이터 정보를 한 번에 불러올 수 있다.
         userDataRef.GetValueAsync()
             .ContinueWithOnMainThread(task =>
             {
@@ -31,9 +40,13 @@ public class DatabaseTester : MonoBehaviour
                     return;
                 }
 
+                // 결과가 성공이었을 경우, 해당 값을 datasnapshot에 저장하여 사용해줄 수 있다.
                 DataSnapshot snapshot = task.Result;
+
+                // 만약 해당 데이터 아래에 정보값이 없었을 경우,
                 if (snapshot.Value == null)
                 {
+                    // 초기값으로 데이터 구성을 세팅한다.
                     UserData userData = new UserData();
                     userData.name = BackendManager.Auth.CurrentUser.DisplayName;
                     userData.email = BackendManager.Auth.CurrentUser.Email;
@@ -46,24 +59,42 @@ public class DatabaseTester : MonoBehaviour
                     string json = JsonUtility.ToJson(userData);
                     userDataRef.SetRawJsonValueAsync(json);
                 }
+                // 아닐 경우,
                 else
                 {
+                    // 저장한 데이터 정보를 읽어들일 수 있는 string 자료형
+                    string json = snapshot.GetRawJsonValue();
+                    // FromJson을 통해 값을 가져와 userData에 적용시켜줄 수도 있다.
+                    UserData userData = JsonUtility.FromJson<UserData>(json);
+
+                    // 현재 플레이어의 레벨 정보값을, 해당 결과 데이터 값의 하위 데이터 경로에서 가져와 사용한다.
                     level = int.Parse(snapshot.Child("level").Value.ToString());
                     Debug.Log(level);
+
+
+                    // 저장된 데이터의 하위 데이터값을 한번에 가져올 수 있는 foreach문
+                    foreach (DataSnapshot child in snapshot.Child("list").Children)
+                    {
+                        Debug.Log($"{child.Key} : {child.Value}");
+                    }
                 }
             });
 
+        // level 데이터값에 변동이 있었을 때, 특정 이벤트를 구독하여 사용해줄 수 있다.
         levelRef.ValueChanged += LevelRef_ValueChanged;
     }
 
     private void OnDisable()
     {
+        // 오브젝트가 비활성화 되었을 때, 특정 이벤트 구독을 해제한다.
         levelRef.ValueChanged -= LevelRef_ValueChanged;
     }
 
     void LevelRef_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         Debug.Log($"값 변경 이벤트 확인 : {e.Snapshot.Value.ToString()}");
+
+        // 이벤트 사용을 통해, level값이 바뀐 결과를 -> level 원본 데이터 값에 실제로 적용시킨다.
         level = int.Parse(e.Snapshot.Value.ToString());
     }
 
@@ -71,18 +102,113 @@ public class DatabaseTester : MonoBehaviour
 
     public void LevelUp()
     {
-        level++;
-        levelRef.SetValueAsync(level);
+        /* 정렬 : OrderByValue
+         * 특정 데이터를 기준으로, 해당 데이터의 하위 데이터를 Value(=정보값)기준 삼아 정렬시켜 불러오는 방식
+         
+        userDataRef.Child("list").OrderByValue().GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled) return;
+            if (task.IsFaulted) return;
+
+            // 불러오는 데에 성공하였을 경우, 해당 불러온 값을 저장한다.
+            DataSnapshot snapshot = task.Result;
+
+            // 위에서 저장해둔 값을 기반으로, 데이터값을 순서대로 콘솔창에 호출한다.
+            foreach (DataSnapshot child in snapshot.Children)
+            {
+                Debug.Log($"{child.Key} : {child.Value}");
+                // 결과 : Value에 등록된 값의 첫글자 자음을 기준으로, 가나다 순으로 정렬되어 값을 불러왔다.
+            }
+        }); */
+
+        /* 정렬 : OrderByChild
+         * UserData를 기준으로, 해당 값 아래로 등록된 데이터 내에서도 특정 데이터의 값을 기준 삼아 정렬시켜 불러오는 방식
+         
+        BackendManager.Database.RootReference.Child("UserData")
+            .OrderByChild("level")
+            // 추가로 아래에 기타 조건 함수를 붙이는 것으로 필터링을 거칠 수 있다.
+            .GetValueAsync()
+            .ContinueWithOnMainThread(task =>
+            {
+              if (task.IsCanceled) return;
+              if (task.IsFaulted) return;
+              DataSnapshot snapshot = task.Result;
+
+              foreach (DataSnapshot child in snapshot.Children)
+              {
+                  Debug.Log($"{child.Key} : {child.Child("level").Value}");
+                  // 결과 : Value에 등록된 level을 작은 순서대로 콘솔창에 호출 하였다.
+              }
+            }); */
+
+        levelRef.SetValueAsync(level++);
     }
 
     public void LevelDown()
     {
-        level--;
-        levelRef.SetValueAsync(level);
+        levelRef.SetValueAsync(level--);
     }
+
+    // === === ===
+
+    public void SaveDefult()
+    {
+        // 기본 자료형을 통한 값의 저장 방식
+        // ~.Child(데이터명).SetValueAsync(실제값(=적용할값));
+        userDataRef.Child("string").SetValueAsync("텍스트");
+        userDataRef.Child("number").SetValueAsync(10);
+        userDataRef.Child("double").SetValueAsync(3.14);
+        userDataRef.Child("bool").SetValueAsync(true);
+    }
+
+    public void SaveList()
+    {
+        // List 자료구조를 통한 값의 순차적인 저장 방식
+        List<string> list = new List<string>() { "첫번째", "두번째", "세번째" };
+        userDataRef.Child("List").SetValueAsync(list);
+    }
+
+    public void SaveDic()
+    {
+        // Dictionary 자료구조를 통한 키&값 저장 방식
+        // Dictionary<데이터명, 실제값(=적용할값)> ~
+        Dictionary<string, object> dictionary = new Dictionary<string, object>()
+
+        {   // 데이터 명(=키) 입력, 값 입력으로 값을 넣어줄 수 있다.
+            { "stringKey", "텍스트" },
+            { "longKey", 10 },
+            { "doubleKey", 3.14 },
+            { "boolKey", true }
+        };
+
+        userDataRef.Child("Dictionary").SetValueAsync(dictionary);
+    }
+
+    public void SaveSetAll()
+    {
+        // 데이터 모두 쓰기 : SetRawJsonValueAsync()
+        // 동시에 여러 자료형을 한 번에 사용하고 싶을 때 사용하는 방식
+
+        // 변경&추가할 값을 저장해둔 클래스를 가져와 선언 후,
+        UserData userData = new UserData();
+
+        //해당 값을 가져와 원하는 정보값을 입력해준다.
+        userData.name = "김전사";
+        userData.email = "yeon14735@gmail.com";
+        userData.level = 100;
+
+        // 후에, 해당 값의 형태를 Json으로의 변경을 진행한다.
+        string json = JsonUtility.ToJson(userData);
+
+        // Json으로 변경된 userData값을 한꺼번에 옮겨준다.
+        userDataRef.SetRawJsonValueAsync(json);
+    }
+
+    // === === ===
 }
 
-[Serializable]
+
+[Serializable] // = 직렬화를 위한 Serializable
 public class UserData
 {
     public string name;
